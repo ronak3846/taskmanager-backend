bf7d493a550193dbde4cdce3027748035093c8ab
unknown <ronakvaishnav99@gmail.com>
2025-07-17 12:04:04 +0530

    front end url change

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const corsOptions = {
   origin: [
     "http://localhost:5173", // local dev
-    "https://taskmanager-frontend-ochre.vercel.app", // Vercel frontend
+    "https://tasksuite.vercel.app/", // Vercel frontend
   ],
   credentials: true,
 };
